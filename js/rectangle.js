87d33e8791f510464a21854a5a7613c999c5aba8
Jordan Feigner <jordan.feigner.r@gmail.com>
2022-07-21 13:45:31 -0700

    CanvasGridCell initialization and drawing

--- js/rectangle.js
+++ js/rectangle.js
@@ -4,17 +4,17 @@ class Rectangle {
     constructor(x, y, width, height) {
         this.x = x;
         this.y = y;
-        this.width = width;
-        this.height = height;
+        this.w = width;
+        this.h = height;
     }
 
     // Boolean function to determine if given (x,y) point lies within the bounds
     // of a Rectangle object
     isPointInBounds(x, y) {
         var x1 = this.x;
-        var x2 = this.x + this.width;
+        var x2 = this.x + this.w;
         var y1 = this.y;
-        var y2 = this.y + this.height;
+        var y2 = this.y + this.h;
         if(x > x1 && x < x2 && y > y1 && y < y2) {
             return true;
         } else {
--- js/script.js
+++ js/script.js
@@ -4,19 +4,31 @@ function main() {
     var app = new MusicSequencer();
     app.container = document.querySelector(".app-container");
 
+    var canvas = document.querySelector(".temp-canvas");
+    canvas.width = 1000;
+    canvas.height = 500;
     var sounds;
 
     app.loadSounds("piano").then(decoded_buffers => {
         sounds = decoded_buffers;
     })
+
+    var grid = new CanvasGrid(canvas, canvas.width, canvas.height, 10, 18);
+    grid.draw();
+
+    var stopper = 0;
 }
 
 class MusicSequencer {
     constructor() {
         this.container = null;
         this.audio_ctx = new AudioContext();
+
+        this.tracks = [];
     }
 
+    // Loads sounds by instrument/directory name
+    // Returns an array of Promises that resolve to decoded sound buffers
     loadSounds(directory) {
         return fetch("php/load_sounds.php?instr=" + directory, {method: "GET"})
         .then(response => response.json())
@@ -33,6 +45,13 @@ class MusicSequencer {
     }
 }
 
+class MusicTrack {
+    constructor(id, instrument, audio_ctx) {
+        this.id = id;
+        this.instrument = instrument;
+    }
+}
+
 class CanvasGrid {
     constructor(canvas, width, height, rows, columns) {
         this.canvas = canvas;
@@ -46,12 +65,44 @@ class CanvasGrid {
         this.row_height = height / rows;
         this.column_width = width / columns;
 
-        this.cells = [];
+        this.cells = this.initCells();
+    }
+
+    initCells() {
+        var cells = [];
+        for(var i = 0; i < this.columns; i++) {
+            var col = [];
+            for(var j = 0; j < this.rows; j++) {
+                var cell_x = this.rect.x + this.column_width * i;
+                var cell_y = this.rect.y + this.row_height * j;
+                col[j] = new CanvasGridCell(cell_x, cell_y, this.column_width, this.row_height);
+            }
+            cells.push(col);
+        }
+        return cells;
+    }
+
+    draw() {
+        this.cells.forEach(column => {
+            column.forEach(cell => {
+                cell.draw(this.ctx);
+            })
+        })
     }
 }
 
 class CanvasGridCell {
     constructor(x, y, width, height) {
-        this.rect = new Rectangle(x, y, w, h);
+        this.rect = new Rectangle(x, y, width, height);
+    }
+
+    draw(ctx) {
+        ctx.lineWidth = 2;
+        ctx.lineStyle = "black";
+
+        ctx.beginPath();
+        ctx.rect(this.rect.x, this.rect.y, this.rect.w, this.rect.h);
+        ctx.stroke();
+        ctx.closePath();
     }
 }
